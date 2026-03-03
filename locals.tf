486c381a8a5939decc136591b46e2ab1a4e62d73
anand k <kanandaiah9@gmail.com>
2026-03-03 22:56:23 +0530

    create

--- locals.tf
+++ locals.tf
@@ -12,12 +12,14 @@ locals {
                      var.vpc_tags
 
   )
-     igw_final_tags = merge(
+    igw_final_tags = merge(
         local.common_tags,
                     {
                         Name = "${var.project}-${var.environment}"
                     },
                      var.igw_tags
 
-     )
+    )
+    az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+
 }
