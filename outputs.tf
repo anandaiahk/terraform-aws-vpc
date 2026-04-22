d3a18e0b56e79a0b8e5eed434be808bfda7087ce
anand k <kanandaiah9@gmail.com>
2026-04-22 19:36:50 +0530

    k8

--- outputs.tf
+++ outputs.tf
@@ -19,3 +19,9 @@ output "private_subnet_ids" {
 output "database_subnet_ids" {
     value = aws_subnet.database[*].id
 }
+
+# modules/vpc/outputs.tf లో add చేయండి
+
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.roboshop.name
+}
